a5946a6885b5b40e5d6055f6ebe888d0409f0374
Cleiton Silva Rego <cleiton_.98@hotmail.com>
2021-11-23 21:23:20 -0300

    rename utils

--- lib/app/micro_core_utils.dart
+++ lib/app/micro_core_utils.dart
@@ -3,4 +3,4 @@ import 'package:flutter/widgets.dart';
 typedef WidgetBuilderArgs = Widget Function(
     BuildContext context, Object object);
 
-final GlobalKey<NavigatorState> navigationKey = GlobalKey();
+final GlobalKey<NavigatorState> navigatorKey = GlobalKey();
